170b1900bf06c0632573f886a1d757c44c413b22
sand32848 <sand32848@hotmail.co.th>
2020-09-03 18:16:58 +0700

    Game mockup update

---
+++ Scene/Gun.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+export var bullet_speed  = 200
+onready var gunSprite = $Sprite
+onready var animationTree = $AnimationTree
+onready var bulletPoint = $Bulletpoint
+var target = Vector2.ZERO
+var bullet = preload("res://Scene/Bullet.tscn")
+
+func _physics_process(delta):
+	target = get_global_mouse_position()
+	look_at(target)
+	if target.x > self.global_position.x :
+		gunSprite.set_flip_v(false)
+	elif target.x < self.global_position.x :
+		gunSprite.set_flip_v(true)
+	var input_vector = Vector2.ZERO
+	
+func shoot():
+		var bullet_instance = bullet.instance()
+		bullet_instance.position = bulletPoint.get_global_position()
+		print(bulletPoint.position)
+		bullet_instance.rotation_degrees = rotation_degrees
+		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed,0).rotated(rotation))
+		get_tree().get_root().add_child(bullet_instance)
+	
+
