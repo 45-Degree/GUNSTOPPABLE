458a50ed0bfc173357607adff0f554fff91bbbb9
sand32848 <sand32848@hotmail.co.th>
2020-10-06 23:24:45 +0700

    Pause update

--- Scene/Levels/Level_1.gd
+++ Scene/Levels/Level_1.gd
@@ -1,6 +1,8 @@
 extends Node
 
 onready var control = $Control
+onready var pause =$Control2
+onready var option = $Control3
 onready var message = $Control/Message
 onready var button = $Control/VBoxContainer/Button
 onready var level = (int(get_tree().current_scene.name) + 1)
@@ -17,8 +19,10 @@ func _process(delta):
 		get_tree().call_group("Detector", "detector_ON")
 		Complete = true
 	if Input.is_action_just_pressed("Pause"):
-		pass
-	
+		get_tree().paused = true
+		pause.show()
+		
+
 func _on_Button_pressed():
 	Singleton.Playable = true
 	if Complete == true and Hostage_dead == false:
@@ -45,3 +49,19 @@ func _on_Detector_body_entered(body):
 		Singleton.Playable = false
 		control.show()
 		get_tree().call_group("Hostage", "invincible")
+
+func _on_Button_button_up():
+	get_tree().paused = false
+	pause.hide()
+
+func _on_Button3_button_up():
+	get_tree().change_scene("res://Scene/UI/MainMenu.tscn")
+	get_tree().paused = false
+
+func _on_Button4_pressed():
+	option.hide()
+	pause.show()
+
+func _on_Button2_button_up():
+	pause.hide()
+	option.show()
