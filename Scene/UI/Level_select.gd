c8f433313385e31e7fdb71266693ef898ef18264
sand32848 <sand32848@hotmail.co.th>
2020-10-31 23:42:46 +0700

    update level select

--- Scene/UI/Level_select.gd
+++ Scene/UI/Level_select.gd
@@ -1,28 +1,13 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _button_pressed(which):
+	print("Button was pressed: ", which.get_name())
+	get_tree().change_scene("res://Scene/Levels/Level_" +str(int(which.name))+ ".tscn")
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Button_button_up():
-	get_tree().change_scene("res://Scene/Levels/Level_1.tscn")
-
-
-func _on_Button2_button_up():
-	get_tree().change_scene("res://Scene/Levels/Level_2.tscn")
-
+	for b in get_node("Control/MarginContainer/VBoxContainer/MarginContainer/HBoxContainer").get_children():
+		b.connect("pressed", self, "_button_pressed",[b])
 
 func _on_buttonQuite_button_up():
 	get_tree().change_scene("res://Scene/UI/MainMenu.tscn")
