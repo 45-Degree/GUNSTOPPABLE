f80db245b431191fc204f1b42fd35e9974e7b617
sand32848 <sand32848@hotmail.co.th>
2021-01-29 10:04:26 +0700

    da

--- Scene/Player/Gun.gd
+++ Scene/Player/Gun.gd
@@ -26,7 +26,7 @@ func shoot():
 		get_tree().current_scene.add_child(bullet_instance)
 #		get_tree().get_root().get_node("World/YSort").add_child(bullet_instance)
 #		get_tree().get_root().add_child(bullet_instance)
-		SoundManager.play_se("res://Sound/SFX/Gunshot/SFX_-_Drum_Machine.wav")
+		SoundManager.play_se("res://Sound/SFX/Gunshot/GunsoundNew.wav")
 		can_fire = false
 		yield(get_tree().create_timer(firerate), "timeout")
 		can_fire = true
