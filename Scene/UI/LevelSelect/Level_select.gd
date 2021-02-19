53a9acdc0dfa571c4b1d82ca6017ed7241c72dbf
sand32848 <sand32848@hotmail.co.th>
2021-02-19 10:17:53 +0700

    Update UI

--- Scene/UI/LevelSelect/Level_select.gd
+++ Scene/UI/LevelSelect/Level_select.gd
@@ -1,12 +1,17 @@
 extends Node
 
 func _button_pressed(which):
-	print("Button was pressed: ", which.get_name())
+	$Transition/AnimationPlayer.play("Wipe_In")
+	yield($Transition/AnimationPlayer,"animation_finished")
 	get_tree().change_scene("res://Scene/Levels/Level_" +str(int(which.name))+ ".tscn")
 
 func _ready():
-	for b in get_node("Control/MarginContainer/VBoxContainer/MarginContainer/GridContainer").get_children():
+	$Transition/AnimationPlayer.play("Wipe_Out")
+	for b in get_node("Control/MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/GridContainer").get_children():
 		b.connect("pressed", self, "_button_pressed",[b])
 
 func _on_buttonQuite_button_up():
-	get_tree().change_scene("res://Scene/UI/MainMenu.tscn")
+	$Transition/AnimationPlayer.play("Wipe_In")
+	yield($Transition/AnimationPlayer,"animation_finished")
+	get_tree().change_scene("res://Scene/UI/MainMenu/MainMenu.tscn")
+
--- Scene/UI/MainMenu/MainMenu.gd
+++ Scene/UI/MainMenu/MainMenu.gd
@@ -2,13 +2,13 @@ extends Node
 
 
 func _ready():
-	$AnimationPlayer.play("Wipe_Out")
+	$Transition/AnimationPlayer.play("Wipe_Out")
 	randomize()
 
 func _on_Button_button_up():
-	$AnimationPlayer.play("Wipe_In")
-	yield($AnimationPlayer,"animation_finished")
-	get_tree().change_scene("res://Scene/UI/Level_select.tscn")
+	$Transition/AnimationPlayer.play("Wipe_In")
+	yield($Transition/AnimationPlayer,"animation_finished")
+	get_tree().change_scene("res://Scene/UI/LevelSelect/Level_select.tscn")
 
 func _on_Button4_button_up():
 	get_tree().quit()
