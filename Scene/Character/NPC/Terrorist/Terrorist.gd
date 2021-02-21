1a54be3c91647a9fbadb8fe1b8cc03d788608af3
sand32848 <sand32848@hotmail.co.th>
2021-02-21 20:34:38 +0700

    Update explosion effect dependency

--- Scene/Character/NPC/Terrorist/Terrorist.gd
+++ Scene/Character/NPC/Terrorist/Terrorist.gd
@@ -1,16 +1,25 @@
 extends KinematicBody2D
 
 export var moving = false
-var ExCe = preload("res://Scene/Effect/Explosion_Celebrate.tscn")
 
 func _physics_process(delta):
+	$Sprite2.play("Idle")
 	if moving == true:
 		$AnimationPlayer.play("Moving")
 
+#func _on_Hurtbox_body_entered(body):
+#	$Sprite2.play("Die")
+#	$CollisionShape2D.disabled = true
+#	yield($Sprite2,"animation_finished")
+#	Singleton.emit_signal("Terrorist_Die")
+#	SoundManager.play_se("res://Sound/SFX/Object/DeadSound.wav")
+#	queue_free()
+
+
 func _on_Hurtbox_area_entered(area):
+	$Sprite2.play("Die")
+	$CollisionShape2D.disabled = true
+	yield($Sprite2,"animation_finished")
 	Singleton.emit_signal("Terrorist_Die")
-	var ExCe_instance = ExCe.instance()
-	ExCe_instance.position = get_global_position()
-	get_tree().get_root().add_child(ExCe_instance)
 	SoundManager.play_se("res://Sound/SFX/Object/DeadSound.wav")
 	queue_free()
