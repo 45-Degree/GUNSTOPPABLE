a5593b22db9e16dc1c268c14da8de8f2d08392bd
sand32848 <sand32848@hotmail.co.th>
2021-03-01 22:31:43 +0700

    Fix level 14,15 inheritance

--- Scene/UI/LevelSelect/Level_select.gd
+++ Scene/UI/LevelSelect/Level_select.gd
@@ -7,8 +7,11 @@ func _button_pressed(which):
 
 func _ready():
 	$Transition/AnimationPlayer.play("Wipe_Out")
-	for b in get_node("Control/MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/GridContainer").get_children():
+	for b in get_node("Control/MarginContainer/VBoxContainer/HBoxContainer/GridContainer").get_children():
 		b.connect("pressed", self, "_button_pressed",[b])
+		var text = b.get_children()
+		for w in text:
+			w.text = str(int(b.name))
 
 func _on_buttonQuite_button_up():
 	$Transition/AnimationPlayer.play("Wipe_In")
