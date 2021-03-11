67834ec247280fa1793eb2c2149f8c9b6fecea0e
sand32848 <sand32848@hotmail.co.th>
2021-03-11 10:30:34 +0700

    Update option

--- Scene/UI/MainMenu/MainMenu.gd
+++ Scene/UI/MainMenu/MainMenu.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 func _ready():
 	$Transition/AnimationPlayer.play("Wipe_Out")
 	randomize()
@@ -14,5 +13,11 @@ func _on_Button4_button_up():
 	get_tree().quit()
 
 func _on_Button3_button_up():
-	get_tree().change_scene("res://Scene/UI/Option.tscn")
+	get_tree().change_scene("res://Scene/UI/Option/Option.tscn")
 
+func _on_Button2_pressed():
+	get_tree().quit()
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Scene/UI/Credit/Credit.tscn")
