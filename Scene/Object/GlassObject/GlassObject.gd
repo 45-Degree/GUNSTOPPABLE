2b316b35080f444addfd6f6698358b24b04db2ec
sand32848 <sand32848@hotmail.co.th>
2021-03-21 20:10:28 +0700

    Update laser

--- Scene/Object/GlassObject/GlassObject.gd
+++ Scene/Object/GlassObject/GlassObject.gd
@@ -3,8 +3,6 @@ onready var animationPlayer = $AnimationPlayer
 export var health = 10
 var invincible = false
 var state = UNSHOT
-export var Laser = false
-export var Emit = false
 export(int, "Left", "Right", "Top", "Bottom") var LaserSpawn
 onready var animatedSprite = $AnimatedSprite
 onready var particle = $Particles2D
@@ -17,51 +15,37 @@ enum{
 }
 
 func _ready():
-		LaserBeam.cast_to = Vector2.ZERO
-		LaserBeam.visible = false
+		LaserBeam.global_position = $BulletSpawnRight.get_global_position()
+		LaserBeam.cast_to = Vector2(40,0)
+#		LaserBeam.cast_to = Vector2.ZERO
+#		LaserBeam.visible = false
 
 func _process(delta):
 	match state:
 		UNSHOT:
-			animatedSprite.hide()
-			animationPlayer.play("Idle")
-			particle.emitting = false
+			pass
+#			LaserBeam.visible = false
+#			LaserBeam.cast_to = Vector2.ZERO
 		SHOTTED:
-			health =- 4
-			animatedSprite.show()
-			animationPlayer.play("Blink")
-			particle.emitting = true
-			yield(get_tree().create_timer(1.5),"timeout")
-	if Laser == false:
-		LaserBeam.enabled = false
-		LaserBeam.visible = false
+			LaserBeam.enabled = true
+			LaserBeam.visible = true
+			if LaserSpawn == 0:
+				LaserBeam.global_position = $BulletSpawnLeft.get_global_position()
+				LaserBeam.cast_to = Vector2(-2000,0)
+			if LaserSpawn == 1:
+				pass
+			if LaserSpawn == 2:
+				LaserBeam.global_position = $BulletSpawnUp.get_global_position()
+				LaserBeam.cast_to = Vector2(0,-2000)
+			if LaserSpawn == 3:
+				LaserBeam.global_position = $BulletSpawnDown.get_global_position()
+				LaserBeam.cast_to = Vector2(0,2000)
 		
 func _on_Hurtbox_area_entered(area):
-	if Laser == true:
-		LaserBeam.enabled = true
-		LaserBeam.visible = true
-		if LaserSpawn == 0:
-			LaserBeam.global_position = $BulletSpawnLeft.get_global_position()
-			LaserBeam.cast_to = Vector2(-2000,0)
-		if LaserSpawn == 1:
-			LaserBeam.global_position = $BulletSpawnRight.get_global_position()
-			LaserBeam.cast_to = Vector2(2000,0)
-		if LaserSpawn == 2:
-			LaserBeam.global_position = $BulletSpawnUp.get_global_position()
-			LaserBeam.cast_to = Vector2(0,-2000)
-		if LaserSpawn == 3:
-			LaserBeam.global_position = $BulletSpawnDown.get_global_position()
-			LaserBeam.cast_to = Vector2(0,2000)
-#
+	state = SHOTTED
+	
 func _on_Hurtbox_area_exited(area):
-	if Laser == true:
-		LaserBeam.visible = false
-		LaserBeam.cast_to = Vector2.ZERO
-
-func _on_Timer_timeout():
-	animatedSprite.hide()
-	animationPlayer.play("Idle")
-	particle.emitting = false
+	state = UNSHOT
 
 
 
