7d247819b5f5ce816bfbaeee01ad2d106e245793
piyaboot <54356200+piyabot@users.noreply.github.com>
2021-04-04 22:09:05 +0700

    Random hostage

---
+++ Scene/Character/NPC/Hostage/LabHostage.gd
@@ -0,0 +1,23 @@
+extends StaticBody2D
+
+signal Hostage_Die
+signal Terrorist_Die
+
+func _ready():
+	connect("Hostage_Die", owner, "_on_Hostage_Die")
+	$AnimatedSprite.play("schIdle" + str(randi() % 3 + 1 ))
+
+func _on_Hurtbox_area_entered(area):
+	Singleton.emit_signal("Hostage_Die")
+	Singleton.hostagePosition = self.global_position
+	var Blood_instance = load("res://Scene/Character/NPC/Hostage/Blood.tscn")
+	var blood_instace = Blood_instance.instance()
+	var world = get_tree().current_scene
+	world.add_child(blood_instace)
+	blood_instace.global_position = global_position
+	emit_signal("Hostage_Die")
+	SoundManager.play_se("res://Scene/Character/NPC/NPC death.wav")
+	queue_free()
+
+func invincible():
+	$Hurtbox/CollisionShape2D.disabled = true
